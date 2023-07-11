ff4f5efa4e9f15e46b9fd77774170fa11a350bc9
Kruthika B Managoli <124071833+kruthikamanagoli@users.noreply.github.com>
2023-07-11 19:26:04 +0530

    Positive_num

---
+++ positive_num.py
@@ -0,0 +1,9 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+for num in list1:
+    if (num % 2 == 0):
+        print(num)
+
+for num in list2:
+    if (num % 2 == 0):
+        print(num)
